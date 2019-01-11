e6ea496413edcfe1fc73ee72d86c4fb425f565a3
Thomas Claydon <tomclaydon102@gmail.com>
2019-01-11 13:04:12 -0500

    Completed actions functionality and MVP

--- api/server.js
+++ api/server.js
@@ -64,9 +64,9 @@ server.get("/api/projects/:projectID/actions", (req, res) => {
       if (actions.length > 0) {
         res.status(200).json(actions);
       } else {
-        res
-          .status(404)
-          .json({ message: `User ID ${projectID} does not contain any Posts.` });
+        res.status(404).json({
+          message: `User ID ${projectID} does not contain any Posts.`
+        });
       }
     })
     .catch(() =>
@@ -81,13 +81,9 @@ server.post("/api/projects", (req, res) => {
   const projectInfo = req.body;
   if (!projectInfo.name || !projectInfo.description) {
     res.status(400).json({
-      errorMessage:
-        "Please provide name || description || completed feilds for the project."
+      errorMessage: "Please provide name || description feilds for the project."
     });
-  } else if (
-    projectInfo.completed === false ||
-    projectInfo.completed === true
-  ) {
+  } else {
     projectDB
       .insert(projectInfo)
       .then(project => {
@@ -159,20 +155,122 @@ server.put("/api/projects/:projectID", (req, res) => {
   }
 });
 
+// actionDB;
 
+// get action by id
+server.get("/api/projects/:projectID/actions/:actionID", (req, res) => {
+  const { projectID, actionID } = req.params;
+  projectDB
+    .getProjectActions(projectID)
+    .then(posts => {
+      if (posts.length > 0) {
+        let postVar = posts[0].postedBy; // check for post id ownership, post id will otherwise show for incorrect user
+        actionDB
+          .get(actionID)
+          .then(post => {
+            if (post.postedBy === postVar) {
+              res.status(200).json(post);
+            } else {
+              res.status(404).json({
+                message: `Post ID ${actionID} does not exist for ${postVar}.`
+              });
+            }
+          })
+          .catch(() =>
+            res
+              .status(500)
+              .json({ error: "The User information could not be retrieved." })
+          );
+      } else {
+        res
+          .status(404)
+          .json({ message: `User ID ${projectID} does not have any Posts.` });
+      }
+    })
+    .catch(() =>
+      res
+        .status(500)
+        .json({ error: "The User information could not be retrieved." })
+    );
+});
 
+// post action
+server.post("/api/projects/:projectID/actions", (req, res) => {
+  const actionInfo = req.body;
+  if (!actionInfo.project_id || !actionInfo.description || !actionInfo.notes) {
+    res.status(400).json({
+      errorMessage:
+        "Please provide project_id || description || notes feilds for the project."
+    });
+  } else {
+    actionDB
+      .insert(actionInfo)
+      .then(actionID => {
+        console.log(actionID);
+        res.status(200).json(actionID);
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: `There was an error while saving the user to the database: ${err}`
+        });
+      });
+  }
+});
 
+// delete action
+server.delete("/api/projects/:projectID/actions/:actionID", (req, res) => {
+  const { actionID } = req.params;
+  actionDB
+    .get(actionID)
+    .then(action => {
+      if (action) {
+        actionDB.remove(actionID).then(() => {
+          res.status(200).json(action);
+        });
+      }
+    })
+    .catch(err => {
+      res.status(404).json({
+        message: `The action with the specified ID does not exist: ${err}`
+      });
+    });
+});
 
+// update action
+server.put("/api/projects/:projectID/actions/:actionID", (req, res) => {
+  const { actionID } = req.params;
+  const actionChanges = req.body;
 
-
-
-
-
-
-
-
-
-
-
+  if (!actionChanges.project_id || !actionChanges.description || !actionChanges.notes) {
+    res.status(400).json({
+      errorMessage:
+        "Please provide project_id || description || notes feilds for the project."
+    });
+  } else {
+    actionDB
+      .get(actionID)
+      .then(action => {
+        if (action) {
+          actionDB
+            .update(actionID, actionChanges)
+            .then(() => {
+              actionDB.get(actionID).then(action => {
+                res.status(200).json(action);
+              });
+            })
+            .catch(() => {
+              res.status(500).json({
+                error: "The action information could not be modified."
+              });
+            });
+        }
+      })
+      .catch(err => {
+        res.status(404).json({
+          message: `The action with the specified ID does not exist: ${err}`
+        });
+      });
+  }
+});
 // export server for index.js use
 module.exports = server;
